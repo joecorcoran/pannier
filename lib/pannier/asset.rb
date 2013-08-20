a4d482bb011440daf5c388ddd91e7640113dccf3
Joe Corcoran <joecorcoran@gmail.com>
2013-08-20 22:20:06 +0100

    Remove tempfile use
    It was overly complex.

--- lib/pannier/asset.rb
+++ lib/pannier/asset.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 module Pannier
   class Asset
 
@@ -5,19 +7,19 @@ module Pannier
 
     def initialize(source_path, result_path, package)
       @source_path, @result_path, @package = source_path, result_path, package
+      @piped_content = File.read(@source_path)
     end
 
     def pipe(&block)
-      self.tmp = yield(tmp)
+      @piped_content = yield(@piped_content)
       self
     end
 
     def write!
       FileUtils.mkdir_p(File.dirname(@result_path))
       File.open(@result_path, 'w+') do |file|
-        file << tmp
+        file << @piped_content
       end
-      clean_up!
     end
 
     def eql?(other)
@@ -27,31 +29,6 @@ module Pannier
     def hash
       @hash ||= File.expand_path(source_path).hash
     end
-
-    private
-
-      def tmp
-        setup_tmp!
-        File.read(tmp_path)
-      end
-
-      def tmp=(contents)
-        File.open(tmp_path, 'w') do |file|
-          file << contents
-        end
-      end
-
-      def tmp_path
-        File.join(@package.source_path, "tmp-#{hash.abs.to_s}")
-      end
-
-      def setup_tmp!
-        self.tmp = File.read(@source_path) unless File.exists?(tmp_path)
-      end
-
-      def clean_up!
-        File.delete(tmp_path)
-      end
 
   end
 end
