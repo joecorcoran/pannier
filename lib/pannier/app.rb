34e806f202a34ba44ce282049d6362e6447d7c01
Joe Corcoran <joecorcoran@gmail.com>
2013-09-11 23:03:52 +0100

    Remove unnecessary method
    No need to tie writing to the app.

--- lib/pannier/app.rb
+++ lib/pannier/app.rb
@@ -39,12 +39,6 @@ module Pannier
       end
     end
 
-    def writer(env = nil)
-      @writer ||= AssetWriter.new(self)
-      @writer.set_env(env) if env
-      @writer
-    end
-
     def process!
       @packages.each(&:process!)
     end
