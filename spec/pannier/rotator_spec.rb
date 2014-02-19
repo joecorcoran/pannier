846d63657966769f6a5bc6dfc90c9c190c3630fd
Joe Corcoran <joecorcoran@gmail.com>
2014-02-19 22:12:04 +0100

    Fix timezone-sensitive spec
    Being outside GMT is eye-opening!

--- spec/pannier/rotator_spec.rb
+++ spec/pannier/rotator_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 require 'pannier/rotator'
 
 describe Pannier::Rotator do
-  let(:rotator) { Pannier::Rotator.new(fixture_path, 5) }
+  let(:limit)   { 3 }
+  let(:rotator) { Pannier::Rotator.new(fixture_path, limit) }
+  let(:timestamps) do
+    time  = Time.new(1984).to_i
+    times = (time..(time + 2))
+    times.map(&:to_s)
+  end
 
   before do
     create_fixtures!
-    time = Time.new(1984).to_i
-    (0..3).each { |i| mkdir("#{time + i}") }
+    timestamps.each { |t| mkdir(t) }
   end
   after { remove_fixtures! }
 
@@ -15,23 +20,22 @@ describe Pannier::Rotator do
     context('always') do
       before { rotator.rotate(Time.new(2000)) }
       it('creates new timestamped directory') do
-        in_fixtures { expect(Dir['*'].sort.last).to eq '946684800' }
+        in_fixtures { expect(Dir['*'].sort.last).to eq Time.new(2000).to_i.to_s }
       end
 
       it('removes oldest timestamped directory') do
-        in_fixtures { expect(Dir['*'].sort.first).to eq '441763200' }
+        in_fixtures { expect(Dir['*'].sort.first).to eq timestamps[1] }
       end
 
       it('maintains limited number of directories') do
-        rotator.rotate(Time.new(2001))
-        in_fixtures { expect(Dir['*'].length).to eq 5 }
+        in_fixtures { expect(Dir['*'].length).to eq limit }
       end
     end
 
     context('with block') do
       it('yields newly pushed directory path') do
         rotator.rotate(Time.new(2000)) do |latest|
-          expect(latest).to match /\/946684800$/
+          expect(latest).to match /\/#{Time.new(2000).to_i}$/
         end
       end
 
@@ -40,7 +44,7 @@ describe Pannier::Rotator do
           rotator.rotate(Time.new(2000)) { |latest| raise RuntimeError }
         rescue RuntimeError
         end
-        in_fixtures { expect(Dir['*'].length).to eq 4 }
+        in_fixtures { expect(Dir['*'].length).to eq limit }
       end
 
       it('raises error if block raises error') do
@@ -56,12 +60,7 @@ describe Pannier::Rotator do
       basenames = rotator.directory_paths.map do |path|
         Pathname.new(path).basename.to_s
       end
-      expect(basenames).to eq [
-        '441763203',
-        '441763202',
-        '441763201',
-        '441763200'
-      ]
+      expect(basenames).to eq timestamps.reverse
     end
   end
 end
