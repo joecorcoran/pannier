0ea30aae3fd2273201e3c1a8e5868edaa15bcbd9
Joe Corcoran <joecorcoran@gmail.com>
2014-03-01 12:25:35 +0100

    Make load method public

--- lib/pannier.rb
+++ lib/pannier.rb
@@ -9,6 +9,15 @@ module Pannier
     App.build(env_name, &block)
   end
 
+  def self.load(path, env_name = 'development')
+    app = build_from(path, env_name)
+    if manifest_exists?(app, env_name)
+      manifest = load_manifest(app, env_name)
+      app.prime!(manifest)
+    end
+    app
+  end
+
   def self.rackup!(ru, path = './.assets.rb')
     app = load(path, ENV['RACK_ENV'])
     ru.map(app.root) { run(app) }
@@ -21,15 +30,6 @@ module Pannier
       config = File.read(path)
       block = eval("proc { #{config} }", TOPLEVEL_BINDING, path, 0)
       App.build(env_name, &block)
-    end
-
-    def self.load(path, env_name = 'development')
-      app = build_from(path, env_name)
-      if manifest_exists?(app, env_name)
-        manifest = load_manifest(app, env_name)
-        app.prime!(manifest)
-      end
-      app
     end
 
     def self.load_manifest(app, env_name)
